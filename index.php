5de52ba14f7bd1b99a97d4f190d985a9f569507a
Daniel Maya <dmaya.calderon@gmail.com>
2020-12-21 17:13:41 +0100

    added index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Gallecoord</title>
+    </head>
+
+    <body>
+
+        <a href="coordinates.php">Coordinates</a>
+        <a href="gallery.php">Coordinates</a>
+
+    </body>
+    
+</html>
